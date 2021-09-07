cfe1d3ed0fd5ab0b73eb84206095515a679d5113
Ali chatraei <m.saeri@fanap.ir>
2021-09-07 08:13:35 +0430

    Install react transition group for animation elemnts

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
 
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-import productReducer from './store/reducers/productReducer';
-import interfaceReducer from './store/reducers/interfaceReducer';
-import orderReducer from './store/reducers/orderReducer';
-import authReducer from './store/reducers/authReducer';
+import productReducer from "./store/reducers/productReducer";
+import interfaceReducer from "./store/reducers/interfaceReducer";
+import orderReducer from "./store/reducers/orderReducer";
+import authReducer from "./store/reducers/authReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -19,14 +19,12 @@ const rootReducer = combineReducers({
   products: productReducer,
   interface: interfaceReducer,
   order: orderReducer,
-  auth: authReducer
+  auth: authReducer,
 });
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
@@ -37,5 +35,5 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
