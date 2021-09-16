fa730de541c04288d9dbdaf70783abde4c5ef6e7
Ali chatraei <m.saeri@fanap.ir>
2021-09-16 11:44:25 +0430

    Create side navigation component in product details

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { filterProducts } from '../../../store/actions';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { filterProducts } from "../../../store/actions";
 
-import NavigationItem from './NavigationItem';
+import NavigationItem from "./NavigationItem";
 
 const navigationItems = ({ filterProducts, isAuth }) => (
   <ul className="nav-list">
+    <NavigationItem link="/" exact>
+      خانه
+    </NavigationItem>
     <NavigationItem
-      clicked={() => filterProducts('female')}
+      clicked={() => filterProducts("female")}
       link="/productlist/female"
-      exact>Women</NavigationItem>
+      exact
+    >
+      زنانه
+    </NavigationItem>
     <NavigationItem
-      clicked={() => filterProducts('male')}
+      clicked={() => filterProducts("male")}
       link="/productlist/male"
-      exact>Men</NavigationItem>
-    <NavigationItem link="/contact" exact>Contact</NavigationItem>
-    <NavigationItem link="/" exact>Home</NavigationItem>
-    {isAuth ? <NavigationItem link="/orders" exact>Orders</NavigationItem> : null}
+      exact
+    >
+      مردانه
+    </NavigationItem>
+    <NavigationItem link="/contact" exact>
+      ارتباط با ما
+    </NavigationItem>
+
+    {isAuth ? (
+      <NavigationItem link="/orders" exact>
+        سفارشات
+      </NavigationItem>
+    ) : null}
   </ul>
 );
 
 NavigationItem.propTypes = {
   isAuth: PropTypes.bool,
-  filterProducts: PropTypes.func
+  filterProducts: PropTypes.func,
 };
 
 const mapStateToProps = ({ auth }) => ({ isAuth: auth.token !== null });
--- src/components/Navigation/NavigationItems/SideNavigation.js
+++ src/components/Navigation/NavigationItems/SideNavigation.js
@@ -1,84 +1,84 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { filterProducts } from '../../../store/actions';
-import '../Navigation.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { filterProducts } from "../../../store/actions";
+import "../Navigation.scss";
 
-import NavigationItem from './NavigationItem';
+import NavigationItem from "./NavigationItem";
 
 const femaleCategories = [
   {
-    category: 'female',
-    content: 'Women',
-    linkType: 'main'
+    category: "female",
+    content: "زنانه",
+    linkType: "main",
   },
   {
-    category: 'women-coats',
-    content: 'Coats'
+    category: "women-coats",
+    content: "کت زنانه",
   },
   {
-    category: 'women-jackets',
-    content: 'Jackets',
+    category: "women-jackets",
+    content: "ژاکت زنانه",
   },
   {
-    category: 'women-suits',
-    content: 'Suits',
+    category: "women-suits",
+    content: "کت و شلوار زنانه",
   },
   {
-    category: 'women-shirts',
-    content: 'Shirts',
+    category: "women-shirts",
+    content: "پیراهن زنانه",
   },
   {
-    category: 'women-t-shirts',
-    content: 'T-shirts',
+    category: "women-t-shirts",
+    content: "تی شرت زنانه",
   },
   {
-    category: 'women-shoes',
-    content: 'Shoes',
+    category: "women-shoes",
+    content: "کفش زنانه",
   },
   {
-    category: 'women-hats',
-    content: 'Hats',
+    category: "women-hats",
+    content: "کلاه زنانه",
   },
   {
-    category: 'male',
-    content: 'Men',
-    linkType: 'main'
+    category: "male",
+    content: "مردانه",
+    linkType: "main",
   },
   {
-    category: 'men-coats',
-    content: 'Coats',
+    category: "men-coats",
+    content: "کت مردانه",
   },
   {
-    category: 'men-jackets',
-    content: 'Jackets',
+    category: "men-jackets",
+    content: "ژاکت مردانه",
   },
   {
-    category: 'men-suits',
-    content: 'Suits',
+    category: "men-suits",
+    content: "کت و شلوار مردانه",
   },
   {
-    category: 'men-shirts',
-    content: 'Shirts',
+    category: "men-shirts",
+    content: "پیراهن مردانه",
   },
   {
-    category: 'men-t-shirts',
-    content: 'T-shirts',
+    category: "men-t-shirts",
+    content: "تی شرت مردانه",
   },
   {
-    category: 'men-shoes',
-    content: 'Shoes',
+    category: "men-shoes",
+    content: "کفش مردانه",
   },
   {
-    category: 'men-hats',
-    content: 'Hats',
+    category: "men-hats",
+    content: "کلاه مردانه",
   },
 ];
 
 const sideNavigation = ({ filterProducts, children }) => (
   <nav className="side-navigation">
     <ul className="side-navigation-list">
-      {femaleCategories.map(femaleCategory => {
+      {femaleCategories.map((femaleCategory) => {
         const { category, linkType, content } = femaleCategory;
 
         return (
@@ -86,10 +86,11 @@ const sideNavigation = ({ filterProducts, children }) => (
             key={category}
             clicked={() => filterProducts(category)}
             linkType={linkType}
-            link={`/productlist/${category}`}>
+            link={`/productlist/${category}`}
+          >
             {content}
           </NavigationItem>
-        )
+        );
       })}
       {children}
     </ul>
@@ -97,7 +98,7 @@ const sideNavigation = ({ filterProducts, children }) => (
 );
 
 sideNavigation.propTypes = {
-  filterProducts: PropTypes.func.isRequired
+  filterProducts: PropTypes.func.isRequired,
 };
 
 export default connect(null, { filterProducts })(sideNavigation);
