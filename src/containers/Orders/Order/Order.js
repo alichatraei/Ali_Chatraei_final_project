457e534e8b5ff32ac9f47154bf4d7dce5523af91
Ali chatraei <m.saeri@fanap.ir>
2021-09-16 10:04:43 +0430

    Edit order component

--- src/containers/Orders/Order/Order.js
+++ src/containers/Orders/Order/Order.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import './Order.scss';
+import React from "react";
+import "./Order.scss";
 
 const order = (props) => {
   return (
     <li className="order-item">
       <ul className="order-products">
-        {props.products.map(product => (
+        {props.products.map((product) => (
           <li key={product.id}>
-            <p className="order-title"><span className="bold">Name: </span>{product.title}</p>
-            <p className="order-title"><span className="bold">Size: </span>{product.size}</p>
-            <p className="order-title"><span className="bold">Amount: </span>{product.amount}</p>
+            <p className="order-title">
+              <span className="bold">نام محصول: </span>
+              {product.title}
+            </p>
+            <p className="order-title">
+              <span className="bold">سایز محصول: </span>
+              {product.size}
+            </p>
+            <p className="order-title">
+              <span className="bold">تعداد محصول: </span>
+              {product.amount}
+            </p>
             <hr />
           </li>
         ))}
       </ul>
-      <p className="order-price">Total Price: {props.price}.00 $</p>
+      <p className="order-price">قیمت کل: {props.price}.۰۰۰ ریال</p>
     </li>
   );
 };
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,66 +1,74 @@
-import React, { Component } from 'react';
-import './Orders.scss';
-import axios from '../../axios';
-import ErrorHandler from '../../hoc/ErrorHandler';
-import { connect } from 'react-redux';
-import * as actions from '../../store/actions';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import "./Orders.scss";
+import axios from "../../axios";
+import ErrorHandler from "../../hoc/ErrorHandler";
+import { connect } from "react-redux";
+import * as actions from "../../store/actions";
+import PropTypes from "prop-types";
 
-import Spinner from '../../components/UI/Spinner/Spinner';
-import Order from './Order/Order';
-import ScrollToTopOnMount from '../../shared/ScrollToTopOnMount';
+import Spinner from "../../components/UI/Spinner/Spinner";
+import Order from "./Order/Order";
+import ScrollToTopOnMount from "../../shared/ScrollToTopOnMount";
 
 class Orders extends Component {
   componentDidMount() {
     this.props.fetchOrders(this.props.token, this.props.userId);
-  };
+  }
 
   render() {
     let orders = <Spinner />;
     if (!this.props.loading) {
-      (this.props.orders.length === 0)
-        ? orders = <p>You do not have any orders yet.</p>
-        : orders = this.props.orders.map(order => (
-          <Order key={order.id} products={order.products} price={order.price} />
-        ))
-    };
+      this.props.orders.length === 0
+        ? (orders = <p>شما تا الان هیچ محصولی ندارید</p>)
+        : (orders = this.props.orders.map((order) => (
+            <Order
+              key={order.id}
+              products={order.products}
+              price={order.price}
+            />
+          )));
+    }
 
     return (
       <>
         <ScrollToTopOnMount />
         <div className="orders-container">
           <h2 className="main-title">Your Orders</h2>
-          <p className="main-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend ligula neque, at faucibus metus trum sedru.</p>
-          <ul className="order-list">
-            {orders}
-          </ul>
+          <p className="main-info">
+            با به تن کردن این محصول به زیبایی خالص خود دست یابید
+          </p>
+          <ul className="order-list">{orders}</ul>
         </div>
       </>
     );
   }
-};
+}
 
 Orders.propTypes = {
   orders: PropTypes.array.isRequired,
   loading: PropTypes.bool.isRequired,
   token: PropTypes.string.isRequired,
   userId: PropTypes.string.isRequired,
-  fetchOrders: PropTypes.func.isRequired
+  fetchOrders: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     orders: state.order.orders,
     loading: state.order.loading,
     token: state.auth.token,
-    userId: state.auth.userId
+    userId: state.auth.userId,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    fetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
+    fetchOrders: (token, userId) =>
+      dispatch(actions.fetchOrders(token, userId)),
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ErrorHandler(Orders, axios));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ErrorHandler(Orders, axios));
